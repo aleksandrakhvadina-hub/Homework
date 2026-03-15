21a82e1f04cadc2cc779d16b0bb2159f6ab6441d
khvadina aleksandra <aleksandra.khvadina@gmail.com>
2026-03-15 12:12:54 +0300

    Homework1

---
+++ src/Homework1/Homework1_1.java
@@ -0,0 +1,9 @@
+package Homework1;
+
+public class Homework1_1 {
+    public static void main(String[] args) {
+        int b = 5;
+        int c = 4;
+        int a = 4*(b+c-1)/2;
+        System.out.println(a);
+    }}
