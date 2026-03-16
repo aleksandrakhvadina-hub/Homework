8d00793a70047a393b2e672844e9261ab3767a2a
khvadina aleksandra <aleksandra.khvadina@gmail.com>
2026-03-16 20:22:52 +0300

    Add code to homework files

--- src/Homework1/Homework1_3.java
+++ src/Homework1/Homework1_3.java
@@ -1,4 +1,12 @@
 package Homework1;
 
 public class Homework1_3 {
-}
+    public static void main(String[] args) {
+        int n = 123;
+        int x = n/10;
+        int b = x%10;
+        int a = n%10;
+        int c = n/100;
+        int y = a+b+c;
+        System.out.println(y);
+    }}
